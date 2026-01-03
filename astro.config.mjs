74c5e9d3d02bd635dba0e4551cc07e9ac1bfa23c
iredox <idreesadam200@gmail.com>
2026-01-03 09:25:33 +0100

    fix: share button

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://technologiya.netlify.app',
   integrations: [react()],
   output: 'server',
   adapter: netlify(),
